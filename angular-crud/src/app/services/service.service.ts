c92b6cd46d0cfb9d43ac2c25b6e8cd2c0b4dfddd
jespinoza <juan.espinoza@fluxit.com.ar>
2020-03-05 12:35:17 -0300

    fix

--- angular-crud/src/app/services/service.service.ts
+++ angular-crud/src/app/services/service.service.ts
@@ -1,5 +1,6 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http'
+import { Persona } from '../models/persona';
 
 @Injectable({
   providedIn: 'root'
@@ -7,4 +8,11 @@ import { HttpClient } from '@angular/common/http'
 export class ServiceService {
 
   constructor( private http:HttpClient ) { }
+
+  private url = 'http://localhost:8080/crud-backend/personas';
+
+  getPersonas(){
+    return this.http.get<Persona[]>(this.url);
+  }
+
 }
