dd447298aaf99d34f2ece68aec8d678ce2098f6c
jespinoza <juan.espinoza@fluxit.com.ar>
2020-03-05 11:55:29 -0300

    Se agregan los componentes y se navega a través del router

--- angular-crud/src/app/app-routing.module.ts
+++ angular-crud/src/app/app-routing.module.ts
@@ -1,10 +1,15 @@
 import { Routes, RouterModule } from '@angular/router';
 import { NgModule } from '@angular/core';
 import { InicioComponent } from './pages/inicio/inicio.component';
+import { AgregarComponent } from './components/agregar/agregar.component';
+import { ListarComponent } from './components/listar/listar.component';
+import { EditarComponent } from './components/editar/editar.component';
 
 export const routes: Routes = [
-    { path: 'inicio', component: InicioComponent },
-    { path: '**',pathMatch: 'full', redirectTo: 'inicio' },
+    { path: 'agregar', component: AgregarComponent },
+    { path: 'editar', component: EditarComponent },
+    { path: 'listar', component: ListarComponent },
+    { path: '**',pathMatch: 'full', redirectTo: 'listar' },
 ]
 
 @NgModule({
--- angular-crud/src/app/app.module.ts
+++ angular-crud/src/app/app.module.ts
@@ -5,12 +5,18 @@ import { AppComponent } from './app.component';
 import { NavbarComponent } from './components/navbar/navbar.component';
 import { InicioComponent } from './pages/inicio/inicio.component';
 import { ROUTES } from './app-routing.module';
+import { ListarComponent } from './components/listar/listar.component';
+import { AgregarComponent } from './components/agregar/agregar.component';
+import { EditarComponent } from './components/editar/editar.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     NavbarComponent,
-    InicioComponent
+    InicioComponent,
+    ListarComponent,
+    AgregarComponent,
+    EditarComponent
   ],
   imports: [
     BrowserModule,
