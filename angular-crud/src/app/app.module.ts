b1326494e1bddfb7707ddd5a6ca9580271914ae3
jespinoza <juan.espinoza@fluxit.com.ar>
2020-03-05 12:08:03 -0300

    Se configura el servicio

--- angular-crud/src/app/app.module.ts
+++ angular-crud/src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { HttpClientModule } from '@angular/common/http';
 
 import { AppComponent } from './app.component';
 import { NavbarComponent } from './components/navbar/navbar.component';
@@ -20,6 +21,7 @@ import { EditarComponent } from './components/editar/editar.component';
   ],
   imports: [
     BrowserModule,
+    HttpClientModule,
     ROUTES
   ],
   providers: [],
