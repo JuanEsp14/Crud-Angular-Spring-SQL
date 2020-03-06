6954777ffbf8dde726e8da5df9c94d42c901b7ac
jespinoza <juan.espinoza@fluxit.com.ar>
2020-03-06 10:00:57 -0300

    Fix URL backend

--- spring.crud/src/main/java/com/spring/crud/PersonaControlador.java
+++ spring.crud/src/main/java/com/spring/crud/PersonaControlador.java
@@ -3,21 +3,34 @@ package com.spring.crud;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @CrossOrigin(origins="http://localhost:4200", maxAge = 3600)
 @RestController
-@RequestMapping({"/personas"})
+@RequestMapping("/personas")
 public class PersonaControlador {
 
 	@Autowired
 	PersonaService service;
 	
-	@GetMapping
+	@GetMapping("/listar")
 	public List<Persona> getAll(){
 		return service.getAll();
 	}
+	
+	@Bean
+    public WebMvcConfigurer corsConfigurer() {
+        return new WebMvcConfigurer() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                registry.addMapping("/**").allowedOrigins("*").allowedMethods("GET", "POST","PUT", "DELETE");
+            }
+        };
+    }
 }
